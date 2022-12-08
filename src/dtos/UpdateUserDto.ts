3d14906702f5a34dc08d0f4dc6049c1c97fab202
julsiam <julsestorco031602@gmail.com>
2022-12-08 11:00:18 +0800

    added Validation in Input

--- src/dtos/UpdateUserDto.ts
+++ src/dtos/UpdateUserDto.ts
@@ -1,4 +1,13 @@
+import { IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
+
 export class UpdateUserDto{ 
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(2)
     title: string;
-    description : string
+
+    @IsNotEmpty()
+    @IsString()
+    @MaxLength(150)
+    description: string;
 }
--- src/dtos/createtask.dto.ts
+++ src/dtos/createtask.dto.ts
@@ -1,4 +1,13 @@
+import { IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
+
 export class CreateNOtesDto{ 
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(2)
     title: string;
-    description : string
+
+    @IsNotEmpty()
+    @IsString()
+    @MaxLength(150)
+    description: string;
 }
